802ad359a6fd756cff9e58b19670b1b195c3f62a
Johannes C. Mayer <j.c.mayer240@gmail.com>
2024-01-17 19:58:43 +0100

    add file share

--- obsidian_pick/cli.py
+++ obsidian_pick/cli.py
@@ -1,18 +1,49 @@
+import os
 from pathlib import Path
 import shutil
 import textwrap
 import subprocess
 import uuid
 
-from click import Path
+from pathlib import Path
 import typer
 import yaml
+from rich import print
+from xdg_base_dirs import xdg_config_home
 
 from obsidian_pick.obsidian import app, iterate_md_files, logger, quartz_content_path, resolve_path, vault_path
 
 app = typer.Typer()
 
+
+# TODO: Use this everywhere
+# TODO: Add saving
+class Config:
+    def __init__(self):
+        config_path = xdg_config_home() / "obsidian_pick" / "config.yaml"
+        config_path.parent.mkdir(parents=True, exist_ok=True)
+        config_path.touch(exist_ok=True)
+        self.config_path = config_path
         
+        self.load_config()
+
+    def load_config(self):
+        with self.config_path.open('r') as f:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+
+        self.quartz_content_path = config.get("quartz_content_path", None)
+        if self.quartz_content_path is None:
+            raise ValueError(f"quartz_content_path is required in the config at {self.config_path}.")
+
+        self.server_content_root = config.get("server_content_root", None)
+        if self.server_content_root is None:
+            raise ValueError("server_content_root is required in the config.")
+
+        server = config.get("server", None)
+        self.file_symlinks = server.get("file_symlinks", [])
+        self.server_redirects = server.get("server_redirects", [])
+    
+
 @app.command()
 def create_htaccess():
     """Create a .htaccess file for the website. in the public folder (you need to build first)"""
@@ -37,21 +68,6 @@ def create_htaccess():
         f.write(htaccess)
 
 
-@app.command()
-def build():
-    """Build the website with quartz."""
-    subprocess.run(["npx", "quartz", "build", "--concurrency", "12"], check=True, cwd='/home/johannes/projects/quartz')
-    create_htaccess()
-
-
-@app.command()
-def deploy(first_build: bool=False):
-    """Copy the build websit to the server."""
-    if first_build:
-        build()
-    subprocess.run(["sudo", "rsync", "-avz", "--delete", "/home/johannes/projects/quartz/public/", "/var/www/html/"], check=True)
-
-
 def copy_to_quartz():
     """Take all the files that are tagged with `publish: true` and copy them to the quartz content folder."""
     shutil.rmtree(quartz_content_path, ignore_errors=True)
@@ -63,15 +79,82 @@ def copy_to_quartz():
         target_path.parent.mkdir(parents=True, exist_ok=True)
         print(f"Copying {f}")
         f.save(target_path)
+        
+    # Copy Media
+    # print("Copying media")
+    # shutil.copytree(vault_path / "_media", quartz_content_path / "_media")
+
+@app.command()
+def new_symlink():
+    """
+    Insert symlinks into the vault.
+    
+    Destinations are relative to the vault root.
+    """
+    pass
+    #TODO
+    # file_links = 
+    # file_links
+
+
+#TODO: Add listing of exsitng symlinks
+#TODO: Add removing of symlinks
+#TODO: Add actual syncing of symlinks (delete deleted ones)
+@app.command()
+def update_server_symlink():
+    """
+    Insert symlinks into the server content folder.
+    
+    Destinations are relative to the server root.
+    """
+    config = Config()
+    for e in config.file_symlinks:
+        src = Path(e['src']).expanduser()
+        dst = Path(e['dst']).expanduser()
+        if not src.exists():
+            logger.warning(f"Warning: Source {src} does not exist. Skipping.")
+            continue
+        dst = config.server_content_root / dst
+        dst.parent.mkdir(parents=True, exist_ok=True)
+        dst.unlink(missing_ok=True)
+        os.symlink(src, dst)
 
 
 @app.command()
-def update_quartz():
+def build():
+    """Build the website with quartz."""
+    print("Copying files to quartz")
     copy_to_quartz()
 
+    print("Building website")
+    subprocess.run(["npx", "quartz", "build", "--concurrency", "12"], check=True, cwd='/home/johannes/projects/quartz')
+
+    print("Generating .htaccess")
+    create_htaccess()
+
 
 @app.command()
-def publish(path: Path):
+def debug_server():
+    print("Copying files to quartz")
+    copy_to_quartz()
+
+    print("Building website")
+    subprocess.run(["npx", "quartz", "build", "--concurrency", "12", "--serve", "--port", "8899"], check=True, cwd='/home/johannes/projects/quartz')
+    
+
+@app.command()
+def deploy(first_build: bool=False):
+    """Copy the build websit to the server."""
+    if first_build:
+        build()
+    subprocess.run(["sudo", "rsync", "-avz", "--delete", "/home/johannes/projects/quartz/public/", "/var/www/html/"], check=True)
+    update_server_symlink()
+
+
+@app.command()
+def publish(path: str):
+    """Set a file to be published."""
+    path = Path(path)
     path = resolve_path(path)
     if path.is_dir():
         print(f"Warning, you provided a directory. We will publish all files in {path}.")
@@ -85,6 +168,16 @@ def publish(path: Path):
         print(f"Publishing {f}")
         f.publish = True
         f.save()
+
+
+@app.command()
+def publish_list():
+    """
+    List all files that are tagged with `publish: true` in their frontmatter.
+    """
+    for f in iterate_md_files():
+        if f.publish:
+            print(f)
 
 
 @app.command()
@@ -106,16 +199,6 @@ def add_permalinks():
             f.frontmatter["id"] = str(id)
             f.frontmatter["permalink"] = str(id)
         f.open("w").write(f"---\n" f"{yaml.dump(f.frontmatter)}\n" f"---\n" f"{f.text}")
-
-
-@app.command()
-def list_publish_tagged():
-    """
-    List all files that are tagged with `publish: true` in their frontmatter.
-    """
-    for f in iterate_md_files():
-        if f.publish:
-            print(f)
 
 
 @app.command()
